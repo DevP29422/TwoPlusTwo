c5d905dc04cc90cce75281803de27854ae7734c2
Dev | GreekSymbol <38300520+DevP29422@users.noreply.github.com>
2020-11-27 21:26:34 -0500

    Add files via upload

---
+++ proto.py
@@ -0,0 +1,25 @@
+num1 = 0
+num2 = 0
+
+def funAdd(parm1, param2):
+    return param2 + parm1
+
+def funSub(param1, param2):
+    return param1 - param2
+
+def funMul(param1, param2):
+    return param1 * param2
+
+def funDiv(param1, param2):
+    return int(param1 / param2)
+
+
+
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+opr = input("Enter oprator: ")
+listOpr = {'+':funAdd(num1, num2), '-':funSub(num1, num2), 
+        'x':funMul(num1, num2), '/':funDiv(num1, num2)}
+return listOpr[opr]
+
